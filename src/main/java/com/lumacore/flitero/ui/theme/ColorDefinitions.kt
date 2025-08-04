955c45f3eaffb3d6abf3632ab1c578d2512d9fd5
MetalChris87 <95472643+MetalChris87@users.noreply.github.com>
2025-08-04 14:13:14 +0100

    Add theme Color definitions

---
+++ src/main/java/com/lumacore/flitero/ui/theme/ColorDefinitions.kt
@@ -0,0 +1,8 @@
+package com.lumacore.flitero.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue500 = Color(0xFF2196F3)
+val Pink500 = Color(0xFFE91E63)
+val Green500 = Color(0xFF4CAF50)
+val Yellow500 = Color(0xFFFFEB3B)
